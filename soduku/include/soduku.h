174099f67ee2715612455c45158f9cc474079e38
zmcken <zmcken@gmail.com>
2013-05-02 22:49:37 +0200

    folder for include and src

---
+++ soduku/include/soduku.h
@@ -0,0 +1,15 @@
+/*
+ * soduku.h
+ *
+ *  Created on: May 2, 2013
+ *      Author: zmc
+ */
+
+#ifndef SODUKU_H_
+#define SODUKU_H_
+
+#include <iostream>
+using namespace std;
+
+
+#endif /* SODUKU_H_ */
---
+++ soduku/src/main.cc
@@ -0,0 +1,18 @@
+/*
+ * main.cc
+ *
+ *  Created on: May 2, 2013
+ *      Author: zmc
+ */
+
+#include <soduku.h>
+
+
+int main() {
+	cout<<"just for test"<<endl;
+
+
+	return 0;
+}
+
+
